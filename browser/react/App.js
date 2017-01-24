dbb28b0f82a94ac66091d8daa828250f115a1985
xpolb1 <bogdan.polovko@gmail.com>
2017-01-24 15:29:30 -0500

    Deployed the socket.io

---
+++ browser/react/App.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+export const App = () => {
+  return (<div></div>)
+}
---
+++ browser/react/index.js
@@ -0,0 +1,10 @@
+import '../socket.io/index'
+
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import {App} from './App';
+
+ReactDOM.render(<App />,document.getElementById('app'));
+
