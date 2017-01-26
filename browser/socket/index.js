4b6c5f19a28f48a15325466f4ae3f32cbb8f30ee
Sam Wheeler <swheeler111@gmail.com>
2017-01-26 16:23:04 -0500

    updated player reducer

--- browser/socket/index.js
+++ browser/socket/index.js
@@ -2,24 +2,21 @@
 window.socket = io(window.location.origin)
 
 import store from '../store';
-import {getPlayersPosition} from '../players/action-creator';
+import { updatePlayerLocations } from '../players/action-creator';
 
 import { initCannon, init, animate } from '../game/main';
 
 socket.on('connect', function() {
   console.log('I have made a persistent two-way connection to the server!')
 
-  // socket.on('move', function() {
-  //   socket.emit('move', {})
-  // })
-
-  socket.on('update_players_position', function(data) {
+  socket.on('update_player_locations', function(data) {
     delete data[socket.id];
     if (data['undefined']) {
       delete data['undefined']
     }
-    store.dispatch(getPlayersPosition(data));
-    console.log(store.getState());
+    console.log('data: ', data)
+    store.dispatch(updatePlayerLocations(data));
+    console.log('state: ', store.getState());
   })
 
   socket.on('start', function(data) {
