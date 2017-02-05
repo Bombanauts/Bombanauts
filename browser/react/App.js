ff216ee5dd6f2d6d105ae8f78ab0d651d40b0dcb
dye784 <damon.ye91@gmail.com>
2017-02-05 15:12:47 -0500

    refactored

--- browser/react/App.js
+++ browser/react/App.js
@@ -18,14 +18,16 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      time: 0
+      time: 0,
+      instructions: true,
     }
+
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     delay(300)
     .then(() => {
-      pointerChecker()
       initCannon()
       init()
       animate()
@@ -37,16 +39,29 @@ class App extends Component {
     })
   }
 
+  handleClick(evt) {
+    evt.stopPropagation()
+
+    const element = document.body
+
+    element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+    element.requestPointerLock();
+
+    controls.enabled = true;
+    this.setState({instructions: false})
+  }
+
   render() {
     return (
       <div>
-        <div id="blocker">
-          <div id="instructions">
+        { this.state.instructions && <div id="blocker" >
+          <div id="instructions" onClick={this.handleClick}>
             <span style={fontStyle}>Click to play</span>
             <br />
             (W,A,S,D = Move, SPACE = Jump, MOUSE = Look, CLICK = Shoot)
           </div>
-        </div>
+        </div> }
             {this.props.dead && <h1  style={{position: "absolute", right: 300}}> YOU ARE FUCKING DEAD</h1>}
             <div style={{position: "absolute", right: 0}}>
              { this.state.time !== 0 &&
@@ -68,37 +83,3 @@ class App extends Component {
 const mapStateToProps = (state) => state.dead
 
 export default connect(mapStateToProps)(App);
-
-export function pointerChecker() {
-  const blocker = document.getElementById( 'blocker' );
-  const instructions = document.getElementById( 'instructions' );
-  const havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
-
-  if ( havePointerLock ) {
-      const element = document.body;
-      const pointerlockchange = function ( event ) {
-          if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
-              controls.enabled = true;
-              blocker.style.display = 'none';
-          } else {
-              controls.enabled = false;
-              blocker.style.display = '-webkit-box';
-              blocker.style.display = '-moz-box';
-              blocker.style.display = 'box';
-              instructions.style.display = '';
-          }
-      }
-      const pointerlockerror = function ( event ) {
-          instructions.style.display = '';
-      }
-      // Hook pointer lock state change events
-      document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-      instructions.addEventListener( 'click', function ( event ) {
-          instructions.style.display = 'none';
-          // Ask the browser to lock the pointer
-          element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-
-              element.requestPointerLock();
-      }, false );
-  }
-}
