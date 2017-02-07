18cf8e31dbb1d7c3685063c7c2793db88ddaf626
Sam Wheeler <swheeler111@gmail.com>
2017-02-07 11:30:44 -0500

    fixed small merge issue

--- server/index.js
+++ server/index.js
@@ -54,13 +54,13 @@ io.on('connection', (socket) => {
   socket.on('get_players', () => {
     socket.emit('get_players', store.getState().players[socket.currentRoom]);
   })
-      
+
   socket.on('set_nickname', (nickname) => {
-      store.dispatch(setNickname(socket.id, nickname, socket.currentRoom))
+    store.dispatch(setNickname(socket.id, nickname, socket.currentRoom))
   })
-      
+
   socket.on('update_world', (data) => {
-    store.dispatch(updatePlayers({ id: data.playerId, position: data.playerPosition, dead: data.dead, nickname: data.nickname}, socket.currentRoom));
+    store.dispatch(updatePlayers({ id: data.playerId, position: data.playerPosition, dead: data.dead, nickname: data.nickname }, socket.currentRoom));
     store.dispatch(updateBombPositions({ userId: data.playerId, bombs: data.playerBombs }, socket.currentRoom))
 
     let newState = convertStateForFrontEnd(store.getState(), socket.currentRoom)
@@ -81,20 +81,20 @@ io.on('connection', (socket) => {
     let currentPlayers = currentState.players[socket.currentRoom];
 
     if (currentPlayers) {
-        let currentPlayersIds = Object.keys(currentPlayers)
-        let currentPlayersLength = currentPlayersIds.length
-        let alivePlayers = []
+      let currentPlayersIds = Object.keys(currentPlayers)
+      let currentPlayersLength = currentPlayersIds.length
+      let alivePlayers = []
 
-       for (let player in currentPlayers) {
-         if (!currentPlayers[player].dead) {
-           currentPlayers[player].socketId = player
-           alivePlayers.push(currentPlayers[player])
-         }
-       }
+      for (let player in currentPlayers) {
+        if (!currentPlayers[player].dead) {
+          currentPlayers[player].socketId = player
+          alivePlayers.push(currentPlayers[player])
+        }
+      }
 
-       if (currentPlayersLength > 1 && alivePlayers.length === 1) {
+      if (currentPlayersLength > 1 && alivePlayers.length === 1) {
         store.dispatch(setWinner(alivePlayers[0].socketId, socket.currentRoom))
-       }
+      }
     }
     let newState = convertStateForFrontEnd(currentState, socket.currentRoom)
     io.in(socket.currentRoom).emit('set_winner', newState.winner)
@@ -112,28 +112,31 @@ io.on('connection', (socket) => {
       store.dispatch(setWinner(null, socket.currentRoom))
       store.dispatch(loadMap(newMap, socket.currentRoom))
       store.dispatch(setTime(currentTime, 185, socket.currentRoom))
-     
+
       let state = store.getState()
-      
+
       io.in(socket.currentRoom).emit('set_winner', store.getState().winner[socket.currentRoom].winner)
       io.in(socket.currentRoom).emit('reset_world', {
         players: state.players[socket.currentRoom],
-        bombs:  {},
+        bombs: {},
         map: state.map[socket.currentRoom],
         timer: state.timer[socket.currentRoom],
         winner: null,
         dead: false
       })
-    }, 5000);  
+    }, 5000);
   })
 
   //remove the player from the state on socket disconnect
   socket.on('disconnect', () => {
     store.dispatch(removePlayer(socket.id, socket.currentRoom))
     store.dispatch(removePlayerBombs(socket.id, socket.currentRoom))
-    let currentStatePlayers = Object.keys(store.getState().players[socket.currentRoom]);
-    if (!currentStatePlayers.length) {
-      store.dispatch(setWinner(null, socket.currentRoom))
+    let currentStatePlayers = store.getState().players[socket.currentRoom];
+    if (currentStatePlayers) {
+      let currentPlayersLength = Object.keys(currentStatePlayers).length;
+      if (!currentPlayersLength) {
+        store.dispatch(setWinner(null, socket.currentRoom))
+      }
     }
     io.in(socket.currentRoom).emit('remove_player', socket.id)
     console.log('socket id ' + socket.id + ' has disconnected. : (');
@@ -145,9 +148,9 @@ app.use(express.static(path.join(__dirname, '..', 'public', 'assets')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 server.listen(port, function() {
-    console.log(`The server is listening on port ${port}!`);
+  console.log(`The server is listening on port ${port}!`);
 });
