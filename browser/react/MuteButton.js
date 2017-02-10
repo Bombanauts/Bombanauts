c51c2c93286b0d1ee46df32cad18c4e50303fca8
dye784 <damon.ye91@gmail.com>
2017-02-10 18:21:05 -0500

    sound button styled

--- browser/react/MuteButton.js
+++ browser/react/MuteButton.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import RaisedButton from 'material-ui/RaisedButton';
 import { muteSound, unmuteSound } from '../redux/sound/action-creator';
+import IconButton from 'material-ui/IconButton';
+
+const styles = {
+  largeIcon: {
+    width: 60,
+    height: 60,
+    fontSize: 60,
+    color: '#fff'
+  },
+  large: {
+    width: 120,
+    height: 120,
+    padding: 30,
+  },
+};
 
 const MuteButton = (props) => {
-  return (
-    <RaisedButton
-          label={props.sound ? 'MUTE' : 'UNMUTE' }
-          style={{ position: 'absolute', left: '5%', bottom: '5%' }}
-          onClick={() => props.sound ? props.muteSound() : props.unmuteSound()}
-          />
-  )
+  if (props.sound) {
+    return (
+      <div style={{ position: 'absolute', left: '1%', bottom: '1%'}}>
+        <IconButton
+          onClick={() => props.muteSound()}
+          disableTouchRipple={true}
+          iconClassName="material-icons"
+          iconStyle={styles.largeIcon}
+          style={styles.large}>
+          volume_up
+        </IconButton>
+      </div>
+    )
+  } else {
+    return (
+      <div style={{ position: 'absolute', left: '1%', bottom: '1%'}}>
+        <IconButton
+          onClick={() => props.unmuteSound()}
+          disableTouchRipple={true}
+          iconClassName="material-icons"
+          iconStyle={styles.largeIcon}
+          style={styles.large}>
+          volume_off
+        </IconButton>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
@@ -23,4 +57,3 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(MuteButton)
-
